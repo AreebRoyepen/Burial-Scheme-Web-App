7ff4d0665cf7380fbf2b6e7792b626af01f78415
Areeb <areeb.royepen@gmail.com>
2020-06-18 13:53:30 +0200

    more on the refactor

--- src/components/modules/shared/CardList.js
+++ src/components/modules/shared/CardList.js
@@ -1,81 +1,277 @@
-import React,{ useState, useEffect } from 'react';
-import { useHistory } from "react-router-dom";
-import LazyLoad from 'react-lazyload';
-import Loading from './LazyLoadingIcon';
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import LazyLoad from "react-lazyload";
+import Loading from "./LazyLoadingIcon";
 import DeletePopOver from "./DeletePopOver";
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "./Alert";
+import { makeStyles } from "@material-ui/core/styles";
+import Api from "../../../api/Api";
 import "../../../styles/eventCard.css";
 import "../../../styles/popUp.css";
 
-export default function CardList (props) {
+const useStyles = makeStyles((theme) => ({
+  typography: {
+    padding: theme.spacing(2),
+  },
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
 
-  const [initialItems, ] = useState(props.content);
+export default function CardList(props) {
+  const classes = useStyles();
+  const [initialItems] = useState(props.content);
   const [items, setItems] = useState(props.content);
   let type = props.type;
   let page = props.page;
   let history = useHistory();
 
-  const [user, setUser] = useState(null)
+  let location = useLocation();
 
-  useEffect( () => {
-    setUser(JSON.parse(localStorage.user))  
-  },[setUser])
+  const [snackBar, openSnackbar] = useState({
+    severity: "",
+    message: "",
+    open: false,
+    time: 0,
+    closeType: null,
+  });
+  const [isSending, setIsSending] = useState(false);
+  const isMounted = useRef(true);
 
-  function filterList  (event){
+  const closeSnack = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    openSnackbar({ ...snackBar, [snackBar.open]: false });
+  };
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.user));
+  }, [setUser]);
+
+  function filterList(event) {
     let items = initialItems;
     items = items.filter((item) => {
-      return JSON.stringify(item).toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+      return (
+        JSON.stringify(item)
+          .toLowerCase()
+          .search(event.target.value.toLowerCase()) !== -1
+      );
     });
     setItems(items);
   }
 
-  const deleteButton = (x) =>{
+  const deleteClicked = useCallback(
+    async (x) => {
+      // don't send again while we are sending
+      if (isSending) return;
 
-    if(user.role.id == 1)
-      return <DeletePopOver style={{float:'right !important'}} content={x} type = {type} className=" card-link u-float-right"/>
-  
-  }
-  
+      // update state
+      setIsSending(true);
+      // send the actual request
+
+      async function fetchData() {
+        var time = 5000;
+
+        if (type === "Member") {
+          let resp = await Api.deleteRequest("v1/members/" + x.id);
+          console.log(resp);
+          if (resp.message === "SUCCESS") {
+            setItems(items.filter((currentItem) => { if (currentItem != x) return (JSON.stringify(currentItem))}))
+            openSnackbar({
+              severity: "success",
+              message: "Successfully Deleted",
+              open: true,
+              time: time,
+              closeType: closeSnack,
+            });
+          } else if (resp.message === "unauthorized") {
+            localStorage.clear();
+            history.push("/", {
+              last: location.pathname,
+              data: location.state,
+            });
+          } else if (resp.message === "error") {
+            time = 6000;
+            openSnackbar({
+              severity: "error",
+              message: "unknown error",
+              open: true,
+              time: time,
+              closeType: closeSnack,
+            });
+          } else if (resp.message === "no connection") {
+            time = 6000;
+            openSnackbar({
+              severity: "error",
+              message: "Check your internet connection",
+              open: true,
+              time: time,
+              closeType: closeSnack,
+            });
+          } else if (resp.message === "timeout") {
+            time = 6000;
+            openSnackbar({
+              severity: "error",
+              message: "Request timed out. Please Try Again",
+              open: true,
+              time: time,
+              closeType: closeSnack,
+            });
+          }
+        } else if (type === "Dependant") {
+          let resp = await Api.deleteRequest("v1/dependants/" + x.id);
+          console.log(resp);
+          if (resp.message === "SUCCESS") {
+            setItems(items.filter((currentItem) => { if (currentItem != x) return (JSON.stringify(currentItem))}))
+            openSnackbar({
+              severity: "success",
+              message: "Successfully Deleted",
+              open: true,
+              time: time,
+              closeType: closeSnack,
+            });
+          } else if (resp.message === "unauthorized") {
+            localStorage.clear();
+            history.push("/", { last: location.pathname });
+          } else if (resp.message === "error") {
+            time = 6000;
+            openSnackbar({
+              severity: "error",
+              message: "unknown error",
+              open: true,
+              time: time,
+              closeType: closeSnack,
+            });
+          } else if (resp.message === "no connection") {
+            time = 6000;
+            openSnackbar({
+              severity: "error",
+              message: "Check your internet connection",
+              open: true,
+              time: time,
+              closeType: closeSnack,
+            });
+          } else if (resp.message === "timeout") {
+            time = 6000;
+            openSnackbar({
+              severity: "error",
+              message: "Request timed out. Please Try Again",
+              open: true,
+              time: time,
+              closeType: closeSnack,
+            });
+          } else {
+            time = 6000;
+            openSnackbar({
+              severity: "warning",
+              message: resp.message,
+              open: true,
+              time: time,
+              closeType: closeSnack,
+            });
+          }
+        }
+      }
+
+      fetchData(x);
+
+      // once the request is sent, update state again
+      if (isMounted.current)
+        // only update if we are still mounted
+        setIsSending(false);
+    },
+    [isSending, location, history]
+  ); // update the callback if the state changes
 
   return (
     <div>
-            
-    <input className ="searchBoxStyle" type="search" placeholder="SEARCH" onChange={ e => filterList(e)}/>
-    <div>
-        
-      {items.map( x =>(
+      <div className={classes.root}>
+        <Snackbar
+          open={snackBar.open}
+          autoHideDuration={snackBar.time}
+          onClose={snackBar.closeType}
+        >
+          <Alert
+            onClose={snackBar.closeType}
+            severity={snackBar.severity}
+          >
+            {snackBar.message}
+          </Alert>
+        </Snackbar>
+      </div>{" "}
 
-        <LazyLoad key = {x.id} placeholder = {<Loading />}>
-        <div key = {x.id}>                              
-          <div className="container"> 
-          <div className="card">
-              <div className="card-body" id ={JSON.stringify(x.active)}>
-                <div className="card-header event-name">
-                    <p>{x.name + " " + x.surname}</p>
-                </div>
-                <span className="card-header">{x.number}<span className="card-header u-float-right"> {x.email} </span>
-                </span>                 
+      <input
+        className="searchBoxStyle"
+        type="search"
+        placeholder="SEARCH"
+        onChange={(e) => filterList(e)}
+      />
+      <div>
+        { items.length >0 ?
+         items.map((x) => (
+          <LazyLoad key={x.id} placeholder={<Loading />}>
+            <div key={x.id}>
+              <div className="container">
+                <div className="card">
+                  <div className="card-body" id={JSON.stringify(x.active)}>
+                    <div className="card-header event-name">
+                      <p>{x.name + " " + x.surname}</p>
+                    </div>
+                    <span className="card-header">
+                      {x.number}
+                      <span className="card-header u-float-right">
+                        {" "}
+                        {x.email}{" "}
+                      </span>
+                    </span>
 
-                  <div className="card-sub-botton card-sub-show">
-                    <input  onClick = {() => { console.log(x.id);  history.push(page,{x:x, edit:true})}} 
-                    type="submit" value="Edit" name="button"className="cardButtons  card-link u-float-right" id={JSON.stringify(x.active)}/>
-                    
-                    {user ?                    
-                    deleteButton(x)
-                    :
-                    <div/>
-                    }                  
-                  
+                    <div className="card-sub-botton card-sub-show">
+                      <input
+                        onClick={() => {
+                          console.log(x.id);
+                          history.push(page, { x: x, edit: true });
+                        }}
+                        type="submit"
+                        value="Edit"
+                        name="button"
+                        className="cardButtons  card-link u-float-right"
+                        id={JSON.stringify(x.active)}
+                      />
+
+                      {user ? (
+                        user.role.id ? (
+                          <DeletePopOver
+                            message={(v) => deleteClicked(v)}
+                            style={{ float: "right !important" }}
+                            content={x}
+                            type={type}
+                            className=" card-link u-float-right"
+                          />
+                        ) : (
+                          <div />
+                        )
+                      ) : (
+                        <div />
+                      )}
+                    </div>
                   </div>
+                </div>
               </div>
-          </div>
-          </div>
-          </div>
-
-        </LazyLoad>
-          
-      ))}
-  </div>
-  </div>
+            </div>
+          </LazyLoad>
+        ))
+      :
+      <div>
+        Nothing to see here
+      </div>}
+      </div>
+    </div>
   );
-    
-};
+}
