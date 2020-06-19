4d7a575cb00a72717cfbb23c9f27ae40600abb7c
Areeb <areeb.royepen@gmail.com>
2020-06-19 15:53:18 +0200

    claim and premium layouts

---
+++ src/components/modules/funds/Claims.js
@@ -0,0 +1,400 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Snackbar from "@material-ui/core/Snackbar";
+import { makeStyles } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
+import Grid from "@material-ui/core/Grid";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import {KeyboardDatePicker} from '@material-ui/pickers';
+import 'date-fns';
+import Alert from "../shared/Alert";
+import Api from "../../../api/Api";
+import "../../../styles/login.css";
+import "../../../styles/login.css";
+import "../../../styles/validationForm.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function Claims() {
+  let location = useLocation();
+  let history = useHistory();
+
+  const classes = useStyles();
+  const [openSnackbar, setOpenSnackbar] = useState({
+    severity: "",
+    message: "",
+    open: false,
+    time: 0,
+    closeType: null,
+  });
+
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const loading = open && options.length === 0;
+  const [isSending, setIsSending] = useState(false);
+  const isMounted = useRef(true);
+
+  const [data, setData] = useState(
+    { 
+      transactionType: Api.CLAIM,
+      buriedDate : new Date(),
+      deathDate: new Date()
+    }
+    );
+
+  const [type, setType] = useState(false);
+
+  const successClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenSnackbar({ ...openSnackbar, [openSnackbar.open]: false });
+  };
+
+  const errorClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSnackbar({ ...openSnackbar, [openSnackbar.open]: false });
+  };
+
+  useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      let resp;
+      if (type) {
+        console.log(resp);
+        resp = await Api.getRequest("v1/members");
+      } else {
+        resp = await Api.getRequest("v1/dependants");
+      }
+      if (resp.message === "SUCCESS") {
+        if (active) {
+          setOptions(resp.data);
+          if (resp.data.length === 0) {
+            setOptions([]);
+          }
+        }
+      } else if (resp.message === "unauthorized") {
+        localStorage.clear();
+        history.push("/", { last: location.pathname });
+      } else {
+        setOpenSnackbar({
+          severity: "error",
+          message: "Check your internet connection",
+          open: true,
+          time: 6000,
+          closeType: errorClose,
+        });
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
+  const payment = useCallback(async () => {
+    // don't send again while we are sending
+    if (isSending) return;
+
+    // update state
+    setIsSending(true);
+
+    // send the actual request
+    async function fetchData() {
+      var time = 3000;
+      let resp;
+      if (type) {
+        resp = await Api.postRequest("v1/claims", data);
+      } else {
+        resp = await Api.postRequest("v1/claims/dependantClaim", data);
+      }
+      console.log(resp)
+      if (resp.message === "SUCCESS") {
+        var message = "Payment Successful";
+        setData({ transactionType: Api.CLAIM });
+        setOpenSnackbar({
+          severity: "success",
+          message: message,
+          open: true,
+          time: time,
+          closeType: successClose,
+        });
+      } else if (resp.message === "unauthorized") {
+        localStorage.clear();
+        history.push("/", { last: location.pathname });
+      } else if (resp.message === "error") {
+        time = 6000;
+        setOpenSnackbar({
+          severity: "error",
+          message: "unknown error",
+          open: true,
+          time: time,
+          closeType: errorClose,
+        });
+      } else if (resp.message === "no connection") {
+        time = 6000;
+        setOpenSnackbar({
+          severity: "error",
+          message: "Check your internet connection",
+          open: true,
+          time: time,
+          closeType: errorClose,
+        });
+      } else if (resp.message === "timeout") {
+        time = 6000;
+        setOpenSnackbar({
+          severity: "error",
+          message: "Request timed out. Please Try Again",
+          open: true,
+          time: time,
+          closeType: errorClose,
+        });
+      }
+    }
+
+    fetchData();
+
+    // once the request is sent, update state again
+    if (isMounted.current)
+      // only update if we are still mounted
+      setIsSending(false);
+  }, [isSending, history, type, location, successClose, errorClose]); // update the callback if the state changes
+
+  const back = () => {
+    setData({ transactionType: Api.CLAIM });
+  };
+
+  // const validateForm = () => {
+  //   var x = {
+  //     ticketNumberF: parseInt(ticketNumberF),
+  //     amount: amount,
+  //     person: reason,
+  //   };
+  //   if (
+  //     /^\d+(\.\d{2})?$/.test(x.amount) &&
+  //     (x.person ||
+  //       (/(null|undefined|^$|^\d+$)/.test(x.ticketNumberF) &&
+  //         x.ticketNumberF > 0))
+  //   ) {
+  //     return "trueValid";
+  //   }
+  //   return "falseValid";
+  // };
+
+  return (
+    <div className="App">
+      {console.log(data)}
+      <div className={classes.root}>
+        <Snackbar
+          open={openSnackbar.open}
+          autoHideDuration={openSnackbar.time}
+          onClose={openSnackbar.closeType}
+        >
+          <Alert
+            onClose={openSnackbar.closeType}
+            severity={openSnackbar.severity}
+          >
+            {openSnackbar.message}
+          </Alert>
+        </Snackbar>
+      </div>
+
+      <body className="bodyVal htmlVal spanVal">
+        <form className="form">
+          <h1 className="h1Dashboard">Claims</h1>
+          <div>
+            <h3>
+              {location.state}
+              <br />
+            </h3>
+            <h4>
+              words here
+              <br />
+            </h4>
+            Select Option
+            <br />
+            <FormControlLabel
+              control={
+                <Grid
+                  component="label"
+                  container
+                  alignItems="center"
+                  spacing={1}
+                >
+                  <Grid item>dependant</Grid>
+                  <Grid item>
+                    <Switch
+                      checked={type}
+                      onChange={(e) => setType(e.target.checked)}
+                      color="#08533C"
+                    />
+                  </Grid>
+                  <Grid item>member</Grid>
+                </Grid>
+              }
+            />
+            <Autocomplete
+              style={{ width: 250, marginBottom: "15px", marginTop: "15px" }}
+              open={open}
+              onOpen={() => {
+                setOpen(true);
+              }}
+              onClose={() => {
+                setOpen(false);
+              }}
+              getOptionSelected={(option, value) => option.name === value.name}
+              getOptionLabel={(option) => option.name + " " + option.surname}
+              options={options}
+              loading={loading}
+              value={data.id}
+              onChange={(event, newValue) => {
+                setData({ ...data, id: newValue.id });
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label={type ? "Select Member" : "Select Dependant"}
+                  variant="outlined"
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <React.Fragment>
+                        {loading ? (
+                          <CircularProgress color="inherit" size={20} />
+                        ) : null}
+                        {params.InputProps.endAdornment}
+                      </React.Fragment>
+                    ),
+                  }}
+                />
+              )}
+            />
+            <TextField
+              style={{ marginTop: "15px" }}
+              id="filled-number"
+              label={"Amount"}
+              type="number"
+              value={data.amount}
+              onChange={(e) => {
+                setData({ ...data, amount: e.target.value });
+              }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+            <TextField
+              id="filled-number"
+              label="Burial Place"
+              rowsMax={10}
+              value={data.burialPlace}
+              onChange={(e) => {
+                setData({ ...data, burialPlace: e.target.value });
+              }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+       
+        <KeyboardDatePicker
+        disableToolbar
+        inputVariant="outlined"
+        variant = "inline"
+        autoOk
+          margin="normal"
+          id="date-picker-dialog"
+          label="Death Date"
+          format="dd/MM/yyyy"
+          value={data.deathDate}
+          onChange={(e) => {
+            setData({ ...data, deathDate: e });
+          }}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+
+<KeyboardDatePicker
+disableToolbar
+inputVariant="outlined"
+variant = "inline"
+autoOk
+          margin="normal"
+          id="date-picker-dialog"
+          label="Burial Date"
+          format="dd/MM/yyyy"
+          value={data.buriedDate}
+          onChange={(e) => {
+            setData({ ...data, buriedDate: e });
+          }}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+            {/* <TextField
+              id="filled-number"
+              
+              rowsMax={10}
+              
+              
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            /> */}
+          </div>
+        </form>
+        <div className="btn-group">
+          {data.amount ? (
+            <button
+              //id={validateForm()}
+              className="button"
+              type="button"
+              disabled={isSending}
+              onClick={payment}
+              style={{ marginTop: "10px" }}
+            >
+              Submit
+            </button>
+          ) : (
+            <div></div>
+          )}
+
+          <button
+            className="button"
+            type="button"
+            onClick={back}
+            style={{ marginTop: "10px" }}
+          >
+            Clear
+          </button>
+        </div>
+      </body>
+    </div>
+  );
+}
---
+++ src/components/modules/funds/Premiums.js
@@ -0,0 +1,313 @@
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Snackbar from "@material-ui/core/Snackbar";
+import { makeStyles } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
+import Grid from "@material-ui/core/Grid";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import {KeyboardDatePicker} from '@material-ui/pickers';
+import 'date-fns';
+import Alert from "../shared/Alert";
+import Api from "../../../api/Api";
+import "../../../styles/login.css";
+import "../../../styles/login.css";
+import "../../../styles/validationForm.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default function Premiums() {
+  let location = useLocation();
+  let history = useHistory();
+
+  const classes = useStyles();
+  const [openSnackbar, setOpenSnackbar] = useState({
+    severity: "",
+    message: "",
+    open: false,
+    time: 0,
+    closeType: null,
+  });
+
+  const [open, setOpen] = useState(false);
+  const [options, setOptions] = useState([]);
+  const loading = open && options.length === 0;
+  const [isSending, setIsSending] = useState(false);
+  const isMounted = useRef(true);
+
+  const [amount, setAmount] = useState();
+  const [person, setPerson] = useState();
+
+  const successClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenSnackbar({ ...openSnackbar, [openSnackbar.open]: false });
+  };
+
+  const errorClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSnackbar({ ...openSnackbar, [openSnackbar.open]: false });
+  };
+
+  useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      let resp = await Api.getRequest("v1/members");
+    
+      if (resp.message === "SUCCESS") {
+        if (active) {
+          setOptions(resp.data);
+          if (resp.data.length === 0) {
+            setOptions([]);
+          }
+        }
+      } else if (resp.message === "unauthorized") {
+        localStorage.clear();
+        history.push("/", { last: location.pathname });
+      } else {
+        setOpenSnackbar({
+          severity: "error",
+          message: "Check your internet connection",
+          open: true,
+          time: 6000,
+          closeType: errorClose,
+        });
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
+
+  const payment = useCallback(async () => {
+    // don't send again while we are sending
+    if (isSending) return;
+
+    // update state
+    setIsSending(true);
+
+    // send the actual request
+    async function fetchData() {
+      var time = 3000;
+
+      let body = {
+
+        amount : amount,
+        type: Api.PREMIUM,
+        id : person.id
+
+      }
+
+      console.log(body)
+
+      let resp = await Api.postRequest("v1/premiums", body);
+      console.log(resp)
+      if (resp.message === "SUCCESS") {
+        var message = "Payment Successful";
+        setOpenSnackbar({
+          severity: "success",
+          message: message,
+          open: true,
+          time: time,
+          closeType: successClose,
+        });
+      } else if (resp.message === "unauthorized") {
+        localStorage.clear();
+        history.push("/", { last: location.pathname });
+      } else if (resp.message === "error") {
+        time = 6000;
+        setOpenSnackbar({
+          severity: "error",
+          message: "unknown error",
+          open: true,
+          time: time,
+          closeType: errorClose,
+        });
+      } else if (resp.message === "no connection") {
+        time = 6000;
+        setOpenSnackbar({
+          severity: "error",
+          message: "Check your internet connection",
+          open: true,
+          time: time,
+          closeType: errorClose,
+        });
+      } else if (resp.message === "timeout") {
+        time = 6000;
+        setOpenSnackbar({
+          severity: "error",
+          message: "Request timed out. Please Try Again",
+          open: true,
+          time: time,
+          closeType: errorClose,
+        });
+      }
+    }
+
+    fetchData();
+
+    // once the request is sent, update state again
+    if (isMounted.current)
+      // only update if we are still mounted
+      setIsSending(false);
+  }, [isSending, history, location, successClose, errorClose]); // update the callback if the state changes
+
+  const back = () => {
+
+  };
+
+  // const validateForm = () => {
+  //   var x = {
+  //     ticketNumberF: parseInt(ticketNumberF),
+  //     amount: amount,
+  //     person: reason,
+  //   };
+  //   if (
+  //     /^\d+(\.\d{2})?$/.test(x.amount) &&
+  //     (x.person ||
+  //       (/(null|undefined|^$|^\d+$)/.test(x.ticketNumberF) &&
+  //         x.ticketNumberF > 0))
+  //   ) {
+  //     return "trueValid";
+  //   }
+  //   return "falseValid";
+  // };
+
+  return (
+    <div className="App">
+      {console.log(amount)}
+      {console.log(person)}
+      <div className={classes.root}>
+        <Snackbar
+          open={openSnackbar.open}
+          autoHideDuration={openSnackbar.time}
+          onClose={openSnackbar.closeType}
+        >
+          <Alert
+            onClose={openSnackbar.closeType}
+            severity={openSnackbar.severity}
+          >
+            {openSnackbar.message}
+          </Alert>
+        </Snackbar>
+      </div>
+
+      <body className="bodyVal htmlVal spanVal">
+        <form className="form">
+          <h1 className="h1Dashboard">Premiums</h1>
+          <div>
+            <h3>
+              {location.state}
+              <br />
+            </h3>
+            <h4>
+              words here
+              <br />
+            </h4>
+            <Autocomplete
+              style={{ width: 250, marginBottom: "15px", marginTop: "15px" }}
+              open={open}
+              onOpen={() => {
+                setOpen(true);
+              }}
+              onClose={() => {
+                setOpen(false);
+              }}
+              getOptionSelected={(option, value) => option.name === value.name}
+              getOptionLabel={(option) => option.name + " " + option.surname}
+              options={options}
+              loading={loading}
+              value={person}
+              onChange={(event, newValue) => {
+                setPerson(newValue);
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label= "Select Member"
+                  variant="outlined"
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <React.Fragment>
+                        {loading ? (
+                          <CircularProgress color="inherit" size={20} />
+                        ) : null}
+                        {params.InputProps.endAdornment}
+                      </React.Fragment>
+                    ),
+                  }}
+                />
+              )}
+            />
+            <TextField
+              style={{ marginTop: "15px" }}
+              id="filled-number"
+              label={"Amount"}
+              type="number"
+              value={amount}
+              onChange={(e) => {
+                setAmount(parseFloat(e.target.value));
+              }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="outlined"
+            />
+
+          </div>
+        </form>
+        <div className="btn-group">
+          {amount ? (
+            <button
+              //id={validateForm()}
+              className="button"
+              type="button"
+              disabled={isSending}
+              onClick={payment}
+              style={{ marginTop: "10px" }}
+            >
+              Submit
+            </button>
+          ) : (
+            <div></div>
+          )}
+
+          <button
+            className="button"
+            type="button"
+            onClick={back}
+            style={{ marginTop: "10px" }}
+          >
+            Clear
+          </button>
+        </div>
+      </body>
+    </div>
+  );
+}
